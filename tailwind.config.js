0331bb79eef62ae6fcd0eb63833aa3f6d0c68c4a
tamagit92 <tamaandria92@gmail.com>
2024-02-12 01:19:00 +0700

    first commid

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
+
+    extend: {
+      colors: {
+        primblue: '#2096ba',
+        primorange: '#df6e21',
+        primgrey: '#c5d2db',
+        fontgrey: '#64748b',
+        fontdark: '#0f172a',
+      },
+
+      screens: {
+        '2xl': '1320px',
+      },
+
+
+    },
+  },
+  plugins: [],
+}
